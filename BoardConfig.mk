ad050a82b64af3d15c08b392e5bed370a47f586f
Ramadhani <ramadhanifals@gmail.com>
2025-07-14 09:19:30 +0700

    onyx: Load adsp firmware
    Change-Id: I11d1cda64c9767fd38fb90ea7328a9e4bd292d69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,7 @@ TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone45/temp" # CPU-0-0-0
 TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko gpr_dlkm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko snd_event_dlkm.ko spf_core_dlkm.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 TW_USE_TOOLBOX := true
