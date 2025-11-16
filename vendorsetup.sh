cc898178db46f95e947fd9ef8a406a76a86bb6d9
AzzyC <azmath2000@gmail.com>
2025-11-16 20:40:55 +0000

    onyx: Brand encrypted builds
    - Identify from OrangeFox banner the recovery variant being used
    
    Change-Id: If2dbcf8eea796675708d3dec0bc8471c22e64d71

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,7 +49,7 @@ if [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_DELETE_AROMAFM=1
 	export FOX_REMOVE_AAPT=1
 	export FOX_DELETE_MAGISK_ADDON=1
-	export FOX_VARIANT="auto-dfe"
+	export FOX_VARIANT="crypto"
 else
 	echo "I: vendorsetup.sh skipped; device mismatch or environment issue."
 fi
